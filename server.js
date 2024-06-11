7ebac2964090a2f8aed061eefa16eadb017deda4
bugkit <bennetty74@gmail.com>
2024-06-11 22:37:22 +0800

    node

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3002;
 
 // Middleware to parse JSON requests
 app.use(express.json());
